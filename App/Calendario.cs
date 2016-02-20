a20b72b500926e5e5a350ec65adebb9c8d5c90b7
alejandromdz <alejandromdz@gmail.com>
2016-02-20 17:12:13 -0600

    Appv.2
    Version Plana

---
+++ App/Calendario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calendar.Net;
+
+namespace App
+{
+    public partial class Calendario : Form
+    {
+        public Calendario()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App/Introduccion3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Introduccion3 : Form
+    {
+        
+
+        public Introduccion3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+
+        }
+    }
+}
