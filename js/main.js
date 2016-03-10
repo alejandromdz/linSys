4ce5fca551001f9fd9907ba1a71ba3f75d126249
alejandromdz <alejandromdz@gmail.com>
2016-03-09 21:38:13 -0600

    contactform5
    Contact Form 5

--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
-$('form.ajax').on('submit', function ()
-{
-    var that = $(this),
-        url = that.attr('action'),
-        method = that.attr('method'),
-        data = {};
-    that.find('[name]').each(function (index, value)
-    {
-        var that = $(this),
-            name = that.attr('name');
-        value = that.val();
-        date[name] = value;
+$(function () {
+    // Get the form.
+    var form = $('#ajax-contact');
 
-    })
-    $.ajax({
-        url: url, type: type, data: data,
-        succes: function (response)
-        {
-            console.log(response);
-        }
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function (event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+
+        .done(function (response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+
+            // Set the message text.
+            $(formMessages).text(response);
+
+            // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        })
+
+        .fail(function (data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
     });
-    console.loh(data);
-    return false;
-}
+});
 
-)
