c0e517398eb96d18596c32cf7a4a62714bfbf931
alejandromdz <alejandromdz@gmail.com>
2016-03-09 22:13:40 -0600

    contactform10
    Contact Form 10

--- contact.php
+++ contact.php
@@ -1,42 +1,4 @@
-<!DOCTYPE html>
-<!--
-	Transit by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Generic - Transit by TEMPLATED</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-	
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-xlarge.css" />
-		
-		
-	</head>
-	<body class="landing">
-	<!-- Header -->
-			<header id="header">
-				<h1><a href="index.html">Inicio</a></h1>
-				<nav id="nav">
-					<ul>
-						<li><a href="index.html">Home</a></li>
-						<li><a href="generic.html">Generic</a></li>
-						<li><a href="elements.html">Elements</a></li>
-						<li><a href="#" class="button special">Sign Up</a></li>
-					</ul>
-				</nav>
-			</header>
-  
+
 
 <?php
 
@@ -89,5 +51,3 @@
     }
 
 ?>
-
-</body>
