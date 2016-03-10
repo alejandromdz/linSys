a53f7bd2290274c5da8c3abe1035caefacbbe3ce
alejandromdz <alejandromdz@gmail.com>
2016-03-09 21:47:05 -0600

    contactform6
    Contact Form 6

--- ajax/contact.php
+++ ajax/contact.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "alejandromdz@hotmail.com";
+        $recipient = "alejandromdz@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
